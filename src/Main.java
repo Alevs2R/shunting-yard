590ecc4bc63fccbfabc3ef8e2019b75dc352b458
Alevs <ac1298@mail.ru>
2017-01-30 18:38:59 +0300

    replace console by reading from input.txt and writing to output.txt

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,31 @@
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         //converting the given arithmetic expression to the reversed polish notation form
 
+        Scanner in = new Scanner(new File("input.txt"));
+
         InfixToPostifx converter = new InfixToPostifx();
-        String expression = "(2*7+4)/3";
-        for(Character ch: expression.toCharArray()){
-            if(!ch.equals(' '))
+
+        for(Character ch: in.nextLine().toCharArray()){
+            if(!ch.equals(' ')) // skip spaces
                 converter.passToken(ch);
         }
         converter.finishExpression();
         String rpn = converter.getExpresion();
-        System.out.println(rpn);
+
+        // now we evaluate given RPN and save the result
 
         RPNCalculator calc = new RPNCalculator();
         calc.setExpression(rpn);
-        System.out.println(calc.calculate());
+
+        PrintWriter writer = new PrintWriter("output.txt");
+        writer.println(String.format(Locale.US, "%.2f", calc.calculate()));
+        writer.close();
     }
 }
